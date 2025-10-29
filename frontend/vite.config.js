47203132a508bb3281eacd17d5704d8c94cf978e
Theway <s230026205@mail.uic.edu.cn>
2025-10-29 16:39:04 +0800

    final version

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,13 +14,13 @@ export default defineConfig({
     proxy: {
       // 代理所有 /api2 请求到Django后端
       '/api2': {
-        target: 'http://172.16.160.100:8000',  // Django后端地址
+        target: 'http://172.16.160.100:8003',  // Django后端地址
         changeOrigin: true,
         rewrite: (path) => path  // 保持路径不变
       },
       // 代理媒体文件请求
       '/media': {
-        target: 'http://localhost:8003',
+        target: 'http://172.16.160.100:8003',
         changeOrigin: true,
         rewrite: (path) => path
       }
